2812a8f591e49d8cd273f39bfc53794fc3bc2e6b
thien20 <81843595+thien20@users.noreply.github.com>
2026-05-04 16:33:06 -0400

    Add: permutations - submission-1

---
+++ Data Structures & Algorithms/permutations/submission-1.py
@@ -0,0 +1,15 @@
+class Solution:
+    def doPermute(self, i):
+        if i == len(self.nums):
+            self.res.append(self.nums[:])
+            return
+        for j in range(i,len(self.nums)):
+            self.nums[i], self.nums[j] = self.nums[j], self.nums[i]
+            self.doPermute(i+1)
+            self.nums[i], self.nums[j] = self.nums[j], self.nums[i]
+
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        self.res = []
+        self.nums = nums
+        self.doPermute(0)
+        return self.res
