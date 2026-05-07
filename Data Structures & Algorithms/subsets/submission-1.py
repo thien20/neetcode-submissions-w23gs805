64ec39ed8a039c1bfa07675c8b6ec249f6c8de54
thien20 <81843595+thien20@users.noreply.github.com>
2026-05-07 00:42:12 -0400

    Add: subsets - submission-1

---
+++ Data Structures & Algorithms/subsets/submission-1.py
@@ -0,0 +1,17 @@
+class Solution:
+    def backtrack(self, i):
+        if i == len(self.nums):
+            self.res.append(self.temp[:])
+            return
+
+        self.temp.append(self.nums[i])
+        self.backtrack(i+1)
+        self.temp.pop()
+        self.backtrack(i+1)
+
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        self.res = []
+        self.temp = []
+        self.nums = nums
+        self.backtrack(0)
+        return self.res
